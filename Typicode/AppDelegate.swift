2ed92c3265ca85ba133f48101f919b0243cc298d
Miso Lubarda <miso.lubarda@here.com>
2018-11-30 17:51:05 +0100

    Use coordinator pattern.
    Adding AppCoordinator with view controller as a placeholder.

--- Typicode/AppDelegate.swift
+++ Typicode/AppDelegate.swift
@@ -10,10 +10,11 @@ import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-    var window: UIWindow?
+    var appCoordinator: AppCoordinator!
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        window = UIWindow(frame: UIScreen.main.bounds)
+        appCoordinator = AppCoordinator()
+        appCoordinator.start()
         return true
     }
 }
---
+++ Typicode/UILayer/AppCoordinator.swift
@@ -0,0 +1,23 @@
+//
+//  AppCoordinator.swift
+//  Typicode
+//
+//  Created by Lubarda, Miso on 30.11.18.
+//  Copyright © 2018 Lubarda, Miso. All rights reserved.
+//
+
+import UIKit
+
+class AppCoordinator {
+    let window: UIWindow
+
+    init() {
+        window = UIWindow(frame: UIScreen.main.bounds)
+    }
+
+    func start() {
+        let vc = UIViewController()
+        window.rootViewController = vc
+        window.makeKeyAndVisible()
+    }
+}
