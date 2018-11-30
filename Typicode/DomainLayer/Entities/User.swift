9c960d91f7d199c5b7164e628ff2b5537391c81c
Miso Lubarda <miso.lubarda@here.com>
2018-11-30 17:57:40 +0100

    Add User as one of the entities.
    User entity is part of our business model.

---
+++ Typicode/DomainLayer/Entities/User.swift
@@ -0,0 +1,16 @@
+//
+//  User.swift
+//  Typicode
+//
+//  Created by Lubarda, Miso on 30.11.18.
+//  Copyright © 2018 Lubarda, Miso. All rights reserved.
+//
+
+import Foundation
+
+struct User {
+    let name: String
+    let username: String
+    let email: String
+    let address: String
+}
