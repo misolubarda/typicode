30cacfc3df138f0e8ddcd533d22d4ca0a0e5bc96
Miso Lubarda <miso.lubarda@here.com>
2018-12-03 20:59:58 +0100

    Add blank PostListViewController

---
+++ Typicode/UILayer/Post list/PostListViewController.swift
@@ -0,0 +1,19 @@
+//
+//  PostListViewController.swift
+//  Typicode
+//
+//  Created by Lubarda, Miso on 03.12.18.
+//  Copyright © 2018 Lubarda, Miso. All rights reserved.
+//
+
+import UIKit
+
+class PostListViewController: UIViewController {
+    init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        return nil
+    }
+}
